ec1ca909e75584434a5318e7091b14fad3f55f65
junior94reis <junior.reisoliveira.1@gmail.com>
2025-08-04 13:35:12 -0300

    previsao de tempo

--- script.js
+++ script.js
@@ -11,8 +11,35 @@ function enviarWhats(event) {
   const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
 
   window.open(url, "_blank");
+};
+
+
+let chave = "cd5597df9d3e0e6bfd6941af49b49e40";
+function colocarNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name + " (" + dados.sys.country + ")" ;
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C";
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description;
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png";
+    return;
+}
+
+async function buscarCidade(cidade){
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    cidade + 
+    "&appid=" + 
+    chave + 
+    "&lang=pt_br" +
+    "&units=metric"
+    )
+    .then(resposta => resposta.json())
+
+    colocarNaTela(dados)
 }
 
 
+function cliqueiNoBotao(){
+   let cidade = document.querySelector(".input-cidade").value
 
-
+   buscarCidade(cidade)
+}
