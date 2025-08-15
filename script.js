c40bb8dfd4a60401d214d9f8f6b8e11f9160f668
junior94reis <junior.reisoliveira.1@gmail.com>
2025-08-15 16:40:27 -0300

    site profissional

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ function enviarWhats(event) {
 
   const nome = document.getElementById("nome").value;
   const mensagem = document.getElementById("mensagem").value;
-  const telefone = "5547997248882";
+  const telefone = "5575991250492";
+
+  const texto = `Olá" Me chamo ${nome}, ${mensagem}`;
+  const msgFormatada = encodeURIComponent(texto);
+
+  const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+  window.open(url, "_blank");
+};
+
+function enviarWhats2(event) {
+    event.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const mensagem = document.getElementById("mensagem").value;
+  const telefone = "5575991250492";
 
   const texto = `Olá" Me chamo ${nome}, ${mensagem}`;
   const msgFormatada = encodeURIComponent(texto);
@@ -14,32 +29,32 @@ function enviarWhats(event) {
 };
 
 
-let chave = "cd5597df9d3e0e6bfd6941af49b49e40";
-function colocarNaTela(dados){
-    console.log(dados)
-    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name + " (" + dados.sys.country + ")" ;
-    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C";
-    document.querySelector(".descricao").innerHTML = dados.weather[0].description;
-    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png";
-    return;
-}
+// let chave = "cd5597df9d3e0e6bfd6941af49b49e40";
+// function colocarNaTela(dados){
+//     console.log(dados)
+//     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name + " (" + dados.sys.country + ")" ;
+//     document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C";
+//     document.querySelector(".descricao").innerHTML = dados.weather[0].description;
+//     document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png";
+//     return;
+// }
 
-async function buscarCidade(cidade){
-    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
-    cidade + 
-    "&appid=" + 
-    chave + 
-    "&lang=pt_br" +
-    "&units=metric"
-    )
-    .then(resposta => resposta.json())
+// async function buscarCidade(cidade){
+//     let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+//     cidade + 
+//     "&appid=" + 
+//     chave + 
+//     "&lang=pt_br" +
+//     "&units=metric"
+//     )
+//     .then(resposta => resposta.json())
 
-    colocarNaTela(dados)
-}
+//     colocarNaTela(dados)
+// }
 
 
-function cliqueiNoBotao(){
-   let cidade = document.querySelector(".input-cidade").value
+// function cliqueiNoBotao(){
+//    let cidade = document.querySelector(".input-cidade").value
 
-   buscarCidade(cidade)
-}
+//    buscarCidade(cidade)
+// }
